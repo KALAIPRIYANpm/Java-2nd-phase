5233b35f15bb5a9c492f57715634f9f29aa4ed42
KALAIPRIYAMpm <kalaipriyan7777@gmail.com>
2025-06-21 15:18:07 +0530

    feat : new

---
+++ src/func.java
@@ -0,0 +1,16 @@
+public class func {
+
+    public static void main(String[] args) {
+
+        String output = greet("hello","World");
+        System.out.println(output);
+
+    }
+
+    public static String greet(String a , String b){
+
+        String add = a +" "+b;
+        return add;
+    }
+
+}
--- src/test.java
+++ src/test.java
@@ -6,5 +6,3 @@ public class test
     }
 
 }
-
-//java
